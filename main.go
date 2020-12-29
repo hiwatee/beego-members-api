68f4f19226856fcb41dd2a300666ac0f822b47a4
hiwatee <m-hiwatashi@kakusin-g.co.jp>
2020-12-29 18:48:38 +0900

    feat: add sql connector

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"beego-members-api/models"
 	_ "beego-members-api/routers"
+	"fmt"
 
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
@@ -21,14 +22,17 @@ func main() {
 func setupDB() {
 	orm.RegisterDriver("mysql", orm.DRMySQL)
 
-	mysqlUser := beego.AppConfig.String("mysqluser")
-	mysqlPass := beego.AppConfig.String("mysqlpass")
-	mysqlHost := beego.AppConfig.String("mysqlurls")
-	mysqldb := beego.AppConfig.String("mysqldb")
+	sqlConn := beego.AppConfig.String("sqlConn")
 
-	orm.RegisterDataBase("default", "mysql", mysqlUser+":"+mysqlPass+"@"+mysqlHost+"/"+mysqldb+"?charset=utf8")
+	orm.RegisterDataBase("default", "mysql", sqlConn)
 
 	orm.RegisterModel(
 		new(models.User),
 	)
+
+	err := orm.RunSyncdb("default", false, true)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 }
