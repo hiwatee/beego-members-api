08f5ceeda346f234bd5a5c635ff427b65630f1cb
hiwatee <m-hiwatashi@kakusin-g.co.jp>
2020-12-30 11:26:33 +0900

    feat: fix connection database

--- main.go
+++ main.go
@@ -1,38 +1,35 @@
 package main
 
 import (
-	"beego-members-api/models"
 	_ "beego-members-api/routers"
 	"fmt"
 
-	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
+	beego "github.com/beego/beego/v2/server/web"
 	_ "github.com/go-sql-driver/mysql"
 )
 
+func init() {
+	sqlConn, err := beego.AppConfig.String("sqlConn")
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	orm.RegisterDataBase("default", "mysql", sqlConn)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// orm.RegisterModel(
+	// 	new(models.User),
+	// )
+	// err = orm.RunSyncdb("default", false, true)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+}
+
 func main() {
-	setupDB()
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
 	beego.Run()
-}
-
-func setupDB() {
-	orm.RegisterDriver("mysql", orm.DRMySQL)
-
-	sqlConn := beego.AppConfig.String("sqlConn")
-
-	orm.RegisterDataBase("default", "mysql", sqlConn)
-
-	orm.RegisterModel(
-		new(models.User),
-	)
-
-	err := orm.RunSyncdb("default", false, true)
-	if err != nil {
-		fmt.Println(err)
-	}
-
 }
