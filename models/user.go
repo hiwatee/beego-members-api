0072702cb59bdaf1219dfa2b49b569c45fe7a4ae
hiwatee <m-hiwatashi@kakusin-g.co.jp>
2021-01-05 19:39:48 +0900

    feat: shape api response

--- models/user.go
+++ models/user.go
@@ -12,15 +12,15 @@ import (
 )
 
 type User struct {
-	Id       int64    `orm:"auto"`
-	Email    string   `orm:"size(128)"`
-	Password string   `orm:"size(128)"`
-	Profile  *Profile `orm:"rel(one)"` // OneToOne relation
+	Id       int64    `orm:"auto" json:"id"`
+	Email    string   `orm:"size(128)" json:"email"`
+	Password string   `orm:"size(128)" json:"-"`
+	Profile  *Profile `orm:"rel(one)" json:"profile"` // OneToOne relation
 }
 
 type Profile struct {
-	Id  int64 `orm:"auto"`
-	Age int16 `orm:"size(128)"`
+	Id  int64 `orm:"auto" json:"id"`
+	Age int16 `orm:"size(128)" json:"age"`
 }
 
 // AddUser insert a new User into database and returns
